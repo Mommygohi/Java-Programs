d35be7e3157dc9b7a223b6cc6e7994394779e657
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2019-11-28 16:24:09 -0800

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
-class void stuff {
+class stuff {
   class static void main(String[] args){
-    
+    String[] board = new String(81);
+    for(int i; i < board.length; i++){
+      board[i] = "empty";
+      System.out.println(board[i]);
+    }
   }
 }
