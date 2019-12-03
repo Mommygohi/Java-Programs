331926ba9e8061a9d0a1289dad4128d93812dc1d
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2019-12-03 10:07:09 -0800

    Update main.java

--- main.java
+++ main.java
@@ -1,15 +1,7 @@
 import java.util.Arrays;
-import java.util.Random;
 
 public class sodoku {
   public static void main(String args[]){
-    int[][] board = new int[9][9];
-    for(int i = 0; i < 9; i++){
-        for(int j = 0; j < 9; j++){
-            Random r = new Random();
-            board[i][j] = r.nextInt((9 - 1) + 1) + 1;
-            System.out.println(board[i][j]);
-        }
-    }
+    
   }
 }
