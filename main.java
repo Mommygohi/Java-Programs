b6a6ca620261bbeee5d05874c954299328003492
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2019-12-03 10:55:43 -0800

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,14 @@
 import java.util.Arrays;
 
-public class sodoku {
-  public static void main(String args[]){
-    
-  }
+public class sodoku{
+     public static void main(String []args){
+        Object[][] board = {new Object[]{1,2,3,4,5,6,7,8,9},new Object[]{1,2,3,4,5,6,7,8,9},new Object[]{1,2,3,4,5,6,7,8,9},new Object[]{1,2,3,4,5,6,7,8,9},new Object[]{1,2,3,4,5,6,7,8,9},new Object[]{1,2,3,4,5,6,7,8,9},new Object[]{1,2,3,4,5,6,7,8,9},new Object[]{1,2,3,4,5,6,7,8,9},new Object[]{1,2,3,4,5,6,7,8,9}};
+        for(int i = 0; i < board.length; i++){
+            String line = "";
+            for(int j = 0; j < board[i].length; j++){
+                line += board[i][j] + " ";
+            }
+            System.out.println(line);
+        }
+     }
 }
