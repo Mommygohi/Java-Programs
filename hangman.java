b33b119e1070ef326646cd49c00f36ff59da5317
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2019-12-10 16:53:35 -0800

    Update hangman.java

--- hangman.java
+++ hangman.java
@@ -1,8 +1,16 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Random;
 
 public class hangman {
+    private static String generatePhrase(){
+        String[] phrases = {"a", "b", "c"};
+        Random rand = new Random();
+        int num = rand.nextInt(2);
+        return phrases[num];
+    }
+    
     public static void main(String[] args){
-        
+        System.out.println(generatePhrase());
     }
 }
